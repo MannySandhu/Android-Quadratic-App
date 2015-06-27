93f33df9117b6d00f97ae0786c828db56f21ea6c
Kiran94 <kiran_patel94@hotmail.com>
2015-06-27 19:12:10 +0100

    refactored and cleaned up home.java

--- QuadraticSolverApplication/app/src/main/java/com/sandhu/manny/quadraticsolverapplication/Home.java
+++ QuadraticSolverApplication/app/src/main/java/com/sandhu/manny/quadraticsolverapplication/Home.java
@@ -2,60 +2,64 @@ package com.sandhu.manny.quadraticsolverapplication;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import static com.sandhu.manny.quadraticsolverapplication.R.id.quadraticQuizButton;
-import static com.sandhu.manny.quadraticsolverapplication.R.id.quadraticSolverButton;
-import static com.sandhu.manny.quadraticsolverapplication.R.id.quadraticTutorialButton;
-
 // home screen activity
-public class Home extends Activity {
+public class Home extends Activity implements View.OnClickListener {
 
     // logging
-    private static final String TAG = "HomeActivity";
+    private final String TAG = "HomeActivity";
+    //Buttons
+    private Button solverButton;
+    private Button tutorialButton;
+    private Button quizButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Home Activity called");
         setContentView(R.layout.activity_home);
 
+        // Link object variables to view ids.
+        solverButton = (Button)findViewById(quadraticSolverButton);
+        tutorialButton = (Button)findViewById(quadraticTutorialButton);
+        quizButton = (Button)findViewById(quadraticQuizButton);
 
-        // wire up widgets to java variables
-        Button solverButton = (Button)findViewById(quadraticSolverButton);
-        Button tutorialButton = (Button)findViewById(quadraticTutorialButton);
-        Button quizButton = (Button)findViewById(quadraticQuizButton);
+        setListeners();
+    }
 
-        // solver button listener
-        solverButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // start a new activity -- go to solver activity
+    //Set onclick listeners for each button.
+    public void setListeners()
+    {
+        solverButton.setOnClickListener(this);
+        tutorialButton.setOnClickListener(this);
+        quizButton.setOnClickListener(this);
+    }
+
+    //Reaction to clicks on screen.
+    public void onClick(View view)
+    {
+        //Depending on the button clicked..
+        switch(view.getId())
+        {
+            case R.id.quadraticQuizButton:
+                Log.d(TAG, "Quadratic Quiz button pressed");
+                //Start quiz activity
+                break;
+            case R.id.quadraticSolverButton:
+                Log.d(TAG, "Quadratic Solver button pressed");
+                // Start Solver activity
                 startActivity(new Intent(Home.this, Solver.class));
-            }
-        });
-
-        // tutorial button listener
-        tutorialButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        // quiz button listener
-        quizButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+                break;
+            case R.id.quadraticTutorialButton:
+                Log.d(TAG, "Quadratic Tutorial button pressed");
+                //Start tutorial activity
+                break;
+        }
 
     }
 
